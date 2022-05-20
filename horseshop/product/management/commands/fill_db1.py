602da256fe2337339cc6f0db44a521bc68daa3c0
Inna Sorina <sig@vpribor.spb.ru>
2022-05-20 17:02:45 +0300

    update fill_db1.py for product.

--- horseshop/product/management/commands/fill_db1.py
+++ horseshop/product/management/commands/fill_db1.py
@@ -2,6 +2,11 @@ from django.core.management.base import BaseCommand
 
 from product.models import (
     Manufacturer,
+    Product,
+)
+
+from category.models import (
+    Subcategory,
 )
 
 
@@ -27,9 +32,41 @@ class Command(BaseCommand):
             'Tommy Hilfiger',
             'Amahorse'
         ]
+
         for company in companies:
             Manufacturer.objects.get_or_create(name=company,
                                                country=contacts[company]["country"],
                                                phone=contacts[company]["phone"],
                                                email=contacts[company]["email"]
                                                )
+        goods = {'saddle pad "COTTON" DR': {'price': 500,
+                                            'count': 29,
+                                            'subcategory': Subcategory(name='Saddle Pads'),
+                                            'manufacturer': Manufacturer(name='Eskadron'),
+                                            'property': ProductProperty()},
+                 'gloves "TRYON"': {'price': 150,
+                                    'count': 8,
+                                    'subcategory': Subcategory(name='Riding Gloves'),
+                                    'manufacturer': Manufacturer(name='Roeckl')},
+                 'breeches "Performans"': {'price': 378,
+                                           'count': 17,
+                                           'subcategory': Subcategory(name='Breeches'),
+                                           'manufacturer': Manufacturer(name='Tommy Hilfiger')},
+                 'helmet "EQUESTRO FRAME"': {'price': 450,
+                                             'count': 3,
+                                             'subcategory': Subcategory(name='Helmets&Hats'),
+                                             'manufacturer': Manufacturer(name='Amahorse')}
+                 }
+        products = [
+            'saddle pad "COTTON" DR',
+            'gloves "TRYON"',
+            'breeches "Performans"',
+            'helmet "EQUESTRO FRAME"'
+        ]
+        for product in products:
+            Product.objects.get_or_create(name=product,
+                                          curr_price=goods[product]["price"],
+                                          curr_count=goods[product]["count"],
+                                          subcategory=goods[product]["subcategory"],
+                                          manufacturer=goods[product]["manufacturer"]
+                                          )
