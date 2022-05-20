db9ae855ab9a5bf0b8bb204f8f3b49fed9d1f9a6
Inna Sorina <sig@vpribor.spb.ru>
2022-05-20 17:04:46 +0300

    add fill_db.py for category.

---
+++ horseshop/category/management/commands/fill_db.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+
+from category.models import (
+    Category,
+    Subcategory,
+)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        subcategories = {'For Rider': ['Riding Gloves',
+                                       'Breeches',
+                                       'Riding Boots',
+                                       'Helmets&Hats',
+                                       'Whips'],  # Хлысты
+                         'For Horse': ['Saddles',  # Седла
+                                       'Saddle Pads'  # Вальтрапы
+                                       'Bridles',  # Уздечки
+                                       'Stirrups',  # Стремена
+                                       ]
+                         }
+        categories = [
+            'For Rider',
+            'For Horse',
+        ]
+
+        for category in categories:
+            Category.objects.get_or_create(name=category)
+            for subcategory in subcategories[category]:
+                SubCategory.objects.get_or_create(name=subcategory,
+                                                  category=Category(name=category))
