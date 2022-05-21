57d4a94fb718c50b950925bcaa2c66d9c2f7da2d
Inna Sorina <sorina-inna@yandex.ru>
2022-05-21 14:56:57 +0300

    fix unused import.

--- horseshop/category/views.py
+++ horseshop/category/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
 
-from category.models import Category
+# from category.models import Category
 from category.models import Subcategory
 
 class SubcategoryListView(ListView):
