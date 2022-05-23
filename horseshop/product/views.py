4daba00f427885b0946a790786d6f09583403505
Inna Sorina <sig@vpribor.spb.ru>
2022-05-23 17:58:06 +0300

    update views and templates for create and display cart.

--- horseshop/product/views.py
+++ horseshop/product/views.py
@@ -11,7 +11,10 @@ from category.models import Subcategory
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     product_to_cart_form = ProductAddToCart()
-    context = {'product': product, 'product_to_cart_form': product_to_cart_form}
+    info_add_product = None
+    context = {'product': product,
+               'product_to_cart_form': product_to_cart_form,
+               'info_add_product': info_add_product}
     return render(request, 'product/product_detail.html', context)
 
 
@@ -28,18 +31,21 @@ def product_list(request, subcategory_slug=None):
     return render(request, 'product/product_list.html', context)
 
 
-# def cart_detail(request, pk):
-#     product_in_cart = ProductInCart.objects.all()
-#     context={'product_in_cart': product_in_cart}
-#     return render(request, 'base.html', {'cart': cart})
+def cart_detail(request):
+    products_in_cart = ProductInCart.objects.all()
+    context={'products_in_cart': products_in_cart}
+    return render(request, 'base.html',context)
 
-# def product_add_to_cart(request, product_pk):
-#     # product = get_object_or_404(Product, pk=product_pk)
-#     # form = ProductAddToCart(request.POST)
-#     # if form.is_valid():
-#     #     cleaned_data = form.cleaned_data
-#     #     product_to_cart.add(product=product, quantity=cleaned_data['quantity'],
-#     #              update_quantity=cleaned_data['update'])
-#     # info_add_product = 'Product has been added to your Cart'
-#     # context={'info_add_product': info_add_product}
-#     return redirect('product:cart_detail', context)
+def product_add_to_cart(request, product_pk):
+    if request.method==POST:
+        if form.is_valid():
+            form = ProductAddToCart(request.POST)
+            cleaned_data = form.cleaned_data
+            count=cleaned_data['quantity']
+            product = get_object_or_404(Product, pk=product_pk)
+            product_to_cart=get_or_create(ProductToCart,product=product)
+            product_to_cart.count+=count
+            product_to_cart.save(update_fields=['count'])
+            info_add_product = 'Product has been added to your Cart'
+    context={'info_add_product': info_add_product}
+    return render(request, 'product/product_detail.html', context)
