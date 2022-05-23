eebc8d1bf51988b26fcc6bb2d2556d5f1053a06c
Inna Sorina <sig@vpribor.spb.ru>
2022-05-23 17:54:08 +0300

    update form for quantity of products to cart

--- horseshop/product/forms.py
+++ horseshop/product/forms.py
@@ -1,13 +1,22 @@
 from django import forms
-from .models import ProductInCart
+# from .models import ProductInCart
 
 
-class ProductAddToCart(forms.ModelForm):
-    class Meta:
-        model = ProductInCart
-        fields = ['total_count', 'color','size']
-        labels = {
-            'total_count': 'Quantity',
-            'color': 'Color',
-            'size': 'Size'
-        }
+# class ProductAddToCart(forms.ModelForm):
+#     class Meta:
+#         model = ProductInCart
+#         fields = ['total_count', 'color','size']
+#         labels = {
+#             'total_count': 'Quantity',
+#             'color': 'Color',
+#             'size': 'Size'
+#         }
+
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 51)]
+
+
+class ProductAddToCart(forms.Form):
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
+                                      coerce=int)
+    update = forms.BooleanField(required=False, initial=False,
+                                widget=forms.HiddenInput)
