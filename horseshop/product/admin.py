96400b1dc1d78d7892935805dafc08eb7189f189
Inna Sorina <sorina-inna@yandex.ru>
2022-05-23 22:57:21 +0300

    fixed admin.py.

--- horseshop/product/admin.py
+++ horseshop/product/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from .models import Product, ProductInOrder, Manufacturer, ProductInCart
 
-# Register your models here.
-
 admin.site.register(Product)
 admin.site.register(ProductInOrder)
 admin.site.register(ProductInCart)
